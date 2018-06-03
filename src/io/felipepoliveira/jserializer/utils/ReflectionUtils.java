a5f2ee9e4f15fcbbe5fa0ed83a7ddfc7b962ae50
Felipe Oliveira <felipeperoliveira@outlook.com>
2018-06-03 09:19:50 -0300

    Launch of dev branch

--- src/io/felipepoliveira/jserializer/utils/ReflectionUtils.java
+++ src/io/felipepoliveira/jserializer/utils/ReflectionUtils.java
@@ -9,11 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import javax.management.RuntimeErrorException;
 
 public class ReflectionUtils {
 	
